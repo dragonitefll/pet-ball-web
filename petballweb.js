18fb5952d05efc5ab2f250ea04a47d799adb70c0
anli5005 <li.anthony917@gmail.com>
2016-10-30 11:41:43 -0400

    Add a joystick for remote driving

--- petballweb.js
+++ petballweb.js
@@ -29,7 +29,7 @@ angular.module("petBallWeb", ["ngMaterial"])
       }
     };
 
-    $scope.signalingChannel = new WebSocket("ws://petball.ward.li:3000");
+    $scope.signalingChannel = new WebSocket("wss://petball.ward.li:3000");
     $scope.signalingChannel.onopen = $scope.handshake;
 
     $scope.needsHandshake = true;
@@ -44,6 +44,8 @@ angular.module("petBallWeb", ["ngMaterial"])
       }
     };
 
+    $scope.inVideoCall = false;
+
     $scope.startVideoCall = function() {
       navigator.getUserMedia(constraints, function(stream) {
         var video = document.getElementById("remote-stream");
@@ -56,7 +58,8 @@ angular.module("petBallWeb", ["ngMaterial"])
           pc.setLocalDescription(offer);
         });
 
-        pc.onnegotiationneeded = function() {
+        pc.onnegotiationneeded = function(e) {
+          console.log(e);
           $scope.signalingChannel.send(JSON.stringify({
             sdp: pc.localDescription,
             token: $scope.token
@@ -86,9 +89,62 @@ angular.module("petBallWeb", ["ngMaterial"])
         };
 
         pc.onaddstream = function(e) {
+          console.log(e);
           window.event = e;
-          video.src = window.URL.createObjectURL(e.stream);
-        }
+          video.srcObject = e.stream;
+
+          $scope.inVideoCall = true;
+          $scope.$apply();
+        };
       }, angular.noop);
     };
+
+    $scope.$watch("joystickData", function(newValue, oldValue) {
+      if ($scope.inVideoCall) {
+        $scope.signalingChannel.send(JSON.stringify({
+          motors: newValue,
+          token: $scope.token
+        }));
+      }
+    })
   })
+  .directive("joystick", function() { return {
+    templateUrl: "joystick.html",
+    restrict: "E",
+    transclude: false,
+    scope: {value: "=ngModel"},
+    controller: function($scope) {
+      $scope.size = 100;
+      $scope.joystickSize = {width: $scope.size + "px", height: $scope.size + "px"};
+      $scope.joystickInnerStyle = {width: $scope.size / 5 + "px", height: $scope.size / 5 + "px"};
+      $scope.mousemove = function(e) {
+        e.preventDefault();
+        if (e.buttons > 0) {
+          var x = e.offsetX - ($scope.size / 2);
+          var y = e.offsetY - ($scope.size / 2);
+
+          var speed = Math.round(Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) * 255 / ($scope.size / 2));
+          var direction = Math.round(Math.atan2(y, x) * 180 / Math.PI) + 90;
+
+          var d = (90 - Math.abs(Math.abs(direction) - 90)) * 17 / 3;
+          var m = (Math.abs(direction) > 90) ? -1 : 1;
+
+          $scope.value = {a: Math.round(speed * m + d), b: Math.round(speed * m - d)};
+
+          $scope.joystickInnerStyle.transition = "none";
+          $scope.joystickInnerStyle.left = e.offsetX - ($scope.size / 10) + "px";
+          $scope.joystickInnerStyle.top = e.offsetY - ($scope.size / 10) + "px";
+        }
+      };
+      $scope.mouseup = function(e) {
+        $scope.value = {a: 0, b: 0};
+        $scope.resetJoystickInner();
+      };
+      $scope.resetJoystickInner = function() {
+        $scope.joystickInnerStyle.transition = "all 0.2s";
+        $scope.joystickInnerStyle.left = $scope.size * 0.4 + "px";
+        $scope.joystickInnerStyle.top = $scope.size * 0.4 + "px";
+      };
+      $scope.mouseup(null);
+    }
+  }})
